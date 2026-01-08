2f24cdd14f25beb6e20a243f00c0516b20e3b25c
Miharin <alvinmiha289@gmail.com>
2026-01-08 19:55:55 +0700

    .

--- WEB/API/pembukuan_api.php
+++ WEB/API/pembukuan_api.php
@@ -446,7 +446,7 @@ function filterPembukuan($id_dokter) {
         error_log("📅 Using ISO format: start=$startDateTime, end=$endDateTime");
         
         // Get filtered pemasukan
-        $pemasukanQuery = "id_dokter=eq.$id_dokter&created_at=gte.$startDateTime&created_at=lte.$endDateTime&select=*&order=created_at.desc";
+        $pemasukanQuery = "id_dokter=eq.$id_dokter&tanggal=gte.$startDateTime&tanggal=lte.$endDateTime&select=*&order=tanggal.desc";
         $pemasukan = supabase('GET', 'pemasukan', $pemasukanQuery);
         
         // ✅ Get filtered pengeluaran - SELECT 'total' FIELD
